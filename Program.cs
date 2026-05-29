0527f2b1d2f2c77851a05c2ba0172eb8c752146d
Emil Aliyev <aliyev.emiw@proton.me>
2026-05-29 04:42:32 +0300

    Added JSON and XML files, implemented XML reader

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Linq;
+
+XDocument doc = XDocument.Load("users.xml");
+
+foreach (var user in doc.Descendants("User"))
+{
+    Console.WriteLine($"Name: {user.Element("Name")?.Value}");
+    Console.WriteLine($"Age: {user.Element("Age")?.Value}");
+    Console.WriteLine($"City: {user.Element("City")?.Value}");
+    Console.WriteLine();
+}
